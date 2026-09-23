c884bfa08f8e92abfe7db99d8b257ef94101f8a8
Ata <atam4834@gmail.com>
2026-09-23 22:15:58 +0100

    .

---
+++ quiz/hello.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func main(){
+ for i := 0; i < 3000; i++{
+ fmt.Println("Ulas Yarramı yeeeeeee")}
+}
+
+
+
--- quiz/quiz.go
+++ quiz/quiz.go
@@ -54,5 +54,10 @@ func ask(s score, question question) score {
 }
 
 func main() {
-	// TODO: Write a quiz program
+qs := questions()
+s := score(0)
+for _, q := range qs {
+        s = ask(s, q)
+    }
+fmt.Println("Final score:", s)
 }
